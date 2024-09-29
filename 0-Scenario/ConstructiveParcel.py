f27a34d3e32209f493fbeb016d3087ee7040c796
Ruisi <ruis6286@gmail.com>
2024-09-30 01:50:34 +0200

    ,,

--- 0-Scenario/ConstructiveParcel.py
+++ 0-Scenario/ConstructiveParcel.py
@@ -2,21 +2,19 @@ import geopandas as gpd
 import pandas as pd
 import numpy as np
 
-lcsf_path = "/Users/rrs/Library/CloudStorage/OneDrive-KTH/KTH/SUPD/0-Degree Project/02-All Codes/ES_Assessment/0-Data/1_predict_newbds_parcels/lcsf.shp"
+resf_path = "/Users/rrs/Library/CloudStorage/OneDrive-KTH/KTH/SUPD/0-Degree Project/02-All Codes/ES_Assessment/0-Data/1_predict_newbds_parcels/resf.shp"
 grundnutzung_path = "/Users/rrs/Library/CloudStorage/OneDrive-KTH/KTH/SUPD/0-Degree Project/02-All Codes/ES_Assessment/0-Data/1_predict_newbds_parcels/grundnutzung.shp"
 
 output_shapefile = "/Users/rrs/Library/CloudStorage/OneDrive-KTH/KTH/SUPD/0-Degree Project/02-All Codes/ES_Assessment/0-Data/1_predict_newbds_parcels/constructive_parcel.shp"
 
 
-parcels = gpd.read_file(lcsf_path)
+parcels = gpd.read_file(resf_path)
 land_types = gpd.read_file(grundnutzung_path)
 
 if parcels.crs != land_types.crs:
     land_types = land_types.to_crs(parcels.crs)
 
-
-# Filter parcels that have a valid EGRID
-parcels_with_egid = parcels.dropna(subset=['GWR_EGID'])
+parcels = parcels.dropna(subset=['EGRIS_EGRI'])
 
 allowed_typ_kommun = [
     '14.1.2', '14.1.3', '11.2.1', '11.2.2', '11.3.1', 
@@ -26,18 +24,18 @@ allowed_typ_kommun = [
 # Filter the land types to only include those with allowed typ_kommun values
 building_land = land_types[land_types['typ_kommun'].isin(allowed_typ_kommun)]
 
-# spatial intersection between parcels with EGID and the filtered building land
-intersected = gpd.overlay(parcels_with_egid, building_land, how='intersection')
+# spatial intersection between parcels with ID and the filtered building land
+intersected = gpd.overlay(parcels, building_land, how='intersection')
 
 # Dissolve by EGID
 intersected_dissolved = intersected.dissolve(by='GWR_EGID')
 
 # Compute area ratio - only significant overlaps are considered
 intersected_dissolved['intersect_area'] = intersected_dissolved.area
-parcels_with_egid['parcel_area'] = parcels_with_egid.area
+parcels['parcel_area'] = parcels.area
 
 # Merge the intersected dissolved data back
-merged = parcels_with_egid.merge(intersected_dissolved[['intersect_area']], how='left', on='GWR_EGID')
+merged = parcels.merge(intersected_dissolved[['intersect_area']], how='left', on='GWR_EGID')
 
 # area ratio
 merged['area_ratio'] = merged['intersect_area'] / merged['parcel_area']
